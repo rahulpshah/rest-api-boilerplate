25ba3014e8b3267340cb8b46596a7a008b5b9b2c
Rahul Shah <123.rahul.shah@gmail.com>
2018-09-09 15:56:24 -0400

    REST api in Node.js

---
+++ node/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const bodyParser = require("body-parser");
+const router = express.Router();
+const port = 3000;
+
+// url: http://localhost:3000/
+app.get('/', (request, response) => response.send('Hello World'));
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+
+// all routes prefixed with /api
+app.use(bodyParser.json());
+app.use('/', router);
+
+// using router.get() to prefix our path
+// url: http://localhost:3000/api/
+router.post('/', (request, response) => {
+        var result = undefined
+	var a = request.body.a
+	var b = request.body.b
+	switch(request.body.operator) {
+	    case "add":
+		res = a + b
+		break;
+	    case "mul":
+		res = a * b
+		break;
+	    default:
+		res = a - b
+	}
+   response.json({result: res+""});
+});
+
+// set the server to listen on port 3000
+app.listen(port, () => console.log(`Listening on port ${port}`));
